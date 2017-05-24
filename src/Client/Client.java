bd6807c781d3ce0596047a476510227583933f06
Piard Brandon <pgm-27@live.fr>
2017-05-24 11:18:36 +0200

    Add communication class

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,7 +1,17 @@
 package Client;
 
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * Created by Brandon on 24/05/2017.
  */
 public class Client {
+
+    Socket socket;
+    InetAddress address;
+
+
+
+
 }
