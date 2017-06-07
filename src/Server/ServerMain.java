a7ce905c23a464a1b1223cc6e36d46ff148631ae
Nejko <Nejko@10.42.130.21>
2017-06-07 09:29:44 +0200

    serverview

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -2,17 +2,11 @@ package Server;
 
 import java.io.IOException;
 
+
 /**
  * Class that will init the servers
  */
 public class ServerMain {
-    public static void main(String[] args) throws IOException 
-	{
-		try {
-			ServerHTTP serverHTTP = new ServerHTTP();
-			
-		}   catch(Exception e){
-			System.out.println("Message error : "+e.getMessage());
-		}
-	}
+
+
 }
--- src/Server/ServerView.java
+++
@@ -1,7 +0,0 @@
-package Server;
-
-/**
- * Created by Brandon on 24/05/2017.
- */
-public class ServerView {
-}
